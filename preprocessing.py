830b78a386b907cebd6b015a4e563994eb017202
MrMeNr1 <d.barenholz@student.tue.nl>
2020-02-10 12:25:02 +0100

    Rewrite preprocessing functions

--- preprocessing.py
+++ preprocessing.py
@@ -1,94 +1,174 @@
-# TODO @ Jens: Check if you agree with how I do this.
 """
 Module for preprocessing a list of sentences.
 Contains methods for tokenisation, stop-word filtering, and stemming.
 """
 
 from typing import List
+from sys import exit
 
-def tokenize(sentences: List[str]) -> List[List[str]]:
+# Provides tokenisation
+def tokenize(sentence: str) -> List[str]:
     """
     Parses and tokenizes a set of sentences and returns a list of lists of tokens, per sentence.
 
     Input:
-        sentences: List of strings.
+        sentence: Single requirement sentence.
     Output:
-        tokens: List of lists, one per string input, consisting of its tokens.
+        result: List of tokens
     """
     # Check if we have a list
-    if not isinstance(sentences, list):
-        print(f"tokenize(): Parameter sentences has type {type(sentences)} but expected {type([])}")
-        exit(1)
+    if not isinstance(sentence, str):
+        exit(f"ERROR! tokenize(): Parameter sentences has type {type(sentence)} but expected {type(str)}")
     
-    if sentences == []:
-        print(f"tokenize(): Parameter sentences is empty. Will return empty list.")
+    if sentence == "":
+        print(f"INFO! tokenize(): Parameter sentences is empty. Will return empty list.")
         return []
     
     # Only import word_tokenize if we actually need to tokenise things
     from nltk import word_tokenize
-        
-    # Create result list
-    result = []
 
-    # For each sentence
-    for sentence in sentences:
-        # Create a tokens list using nltk
-        sentence_tokens = word_tokenize(sentence)
+    return word_tokenize(sentence)
 
-        # Add tokens for this sentence to the results list.
-        result.append(sentence_tokens)
+# Provides stop word filtering
+def remove_stop_words(tokens: List[str]) -> List[str]:
+    """
+    Removes stop words from a list of tokens.
 
-    # After tokenisation, return the result list in both normal and flattened manner
-    # TODO: Remove flattened results if not needed.
-    flattened_results = [token for sentence_list in result for token in sentence_list]
-
-    if flattened_results == []:
-        print(f"tokenize(): Somehow, results contains only empty lists.")
-
-    return result
-
-def remove_stop_words(tokenised_sentences: List[List[str]]) -> List[List[str]]:
-    # TODO: Implement method.
-    from nltk.corpus import stopwords
-
-    # Create filtering set for removign stop words
-    filter_set = set(stopwords.words('english'))
-
-    # Possibly add custom words
-    custom_filter_set = set("")
-    filter_set = filter_set.union(custom_filter_set)
-
-    # Create result list
-    result = []
-
-    # Perform stop word removal for each tokenised sentence
-    for tokenised_sentence in tokenised_sentences:
-        # Only add tokens that are not present in the filter set.
-        filtered_sentence = [token for token in tokenised_sentence if not token in filter_set]
-        
-        # Add filtered sentence to result
-        result.append(filtered_sentence)
+    Input:
+        tokens: List of tokens
+    Output:
+        result: List of filtered tokens
+    """
+    # Create filtering set for removing stop words
+    filter_set = set()
+    
+    # Read stopwords from file
+    with open('/stopwords.txt', 'r') as f:
+        for word in f.readlines():
+            filter_set.add(word.rstrip())
 
     # Return filtered sentences
-    return result
+    return [token for token in tokens if not token in filter_set]
 
-def stem(tokenised_and_filtered_sentences: List[List[str]]) -> List[List[str]]:
-    # TODO: Implement method
+# Provides stemming
+def stem(tokens: List[str]) -> List[str]:
+    """
+    Reduce tokens to their root form.
+
+    Input:
+        tokens: List of tokens.
+    Output:
+        result: List of root form of tokens.
+    """
     from nltk.stem import PorterStemmer
 
     # Create a PorterStemmer
     stemmer = PorterStemmer()
 
-    # Create result list
-    result = []
+    return [stemmer.stem(token) for token in tokens]
 
-    # Perform stemming for each filtered tokenised sentence
-    for sentence in tokenised_and_filtered_sentences:
-        # Only add tokens that are not present in the filter set.
-        stemmed_sentence = [stemmer.stem(token) for token in sentence]
+
+#### OLD CODE: Works on lists, as opposed to single string. Daniel did goof.
+
+# # Provides tokenisation
+# def _tokenize(sentences: List[str]) -> List[List[str]]:
+#     """
+#     Parses and tokenizes a set of sentences and returns a list of lists of tokens, per sentence.
+
+#     Input:
+#         sentences: List of sentences in form 'str' per sentence.
+#     Output:
+#         result: List of tokenised sentences in form 'List[str]' per sentence
+#     """
+#     # Check if we have a list
+#     if not isinstance(sentences, list):
+#         exit(f"ERROR! tokenize(): Parameter sentences has type {type(sentences)} but expected {type(list)}")
+    
+#     if sentences == []:
+#         print(f"INFO! tokenize(): Parameter sentences is empty. Will return empty list.")
+#         return []
+    
+#     # Only import word_tokenize if we actually need to tokenise things
+#     from nltk import download
+#     from nltk import word_tokenize
+
+#     # Download the resources for tokenisation (tokenizers/punkt/PY3/english.pickle)
+#     download('punkt')
+
+#     # Create result list
+#     result = []
+
+#     # For each sentence
+#     for sentence in sentences:
+#         # Create a tokens list using nltk
+#         sentence_tokens = word_tokenize(sentence)
+
+#         # Add tokens for this sentence to the results list.
+#         result.append(sentence_tokens)
+
+#     # Extra check for flattened list.
+#     if [token for sentence_list in result for token in sentence_list] == []:
+#         print(f"INFO! tokenize(): Somehow, results contains only empty lists.")
+
+#     return result
+
+# # Provides stop word filtering
+# def _remove_stop_words(tokenised_sentences: List[List[str]]) -> List[List[str]]:
+#     """
+#     Removes stop words from a list of tokenised sentences, and returns the results.
+
+#     Input:
+#         tokenised_sentences: List of tokenised sentences in form 'List[str]' per sentence
+#     Output:
+#         result: List of tokenised sentences without stop words, in form 'List[str]' per sentence
+#     """
+#     # Create filtering set for removing stop words
+#     filter_set = set()
+    
+#     # Read stopwords from file
+#     with open('/stopwords.txt', 'r') as f:
+#         for word in f.readlines:
+#             filter_set.add(word.rstrip())
+
+#     # Create result list
+#     result = []
+
+#     # Perform stop word removal for each tokenised sentence
+#     for tokenised_sentence in tokenised_sentences:
+#         # Only add tokens that are not present in the filter set.
+#         filtered_sentence = [token for token in tokenised_sentence if not token in filter_set]
         
-        # Add filtered sentence to result
-        result.append(stemmed_sentence)
+#         # Add filtered sentence to result
+#         result.append(filtered_sentence)
 
-    # Return stemmed sentences
-    return result
+#     # Return filtered sentences
+#     return result
+
+# # Provides stemming
+# def _stem(tokenised_and_filtered_sentences: List[List[str]]) -> List[List[str]]:
+#     """
+#     Reduce tokens to their root form.
+
+#     Input:
+#         tokenised_and_filtered_sentences: List of tokenised sentences without stop words in form 'List[str]' per sentence
+#     Output:
+#         result: List of root forms of tokenised sentences without stop words, in form 'List[str]' per sentence
+#     """
+#     from nltk.stem import PorterStemmer
+
+#     # Create a PorterStemmer
+#     stemmer = PorterStemmer()
+
+#     # Create result list
+#     result = []
+
+#     # Perform stemming for each filtered tokenised sentence
+#     for sentence in tokenised_and_filtered_sentences:
+#         # Only add tokens that are not present in the filter set.
+#         stemmed_sentence = [stemmer.stem(token) for token in sentence]
+        
+#         # Add filtered sentence to result
+#         result.append(stemmed_sentence)
+
+#     # Return stemmed sentences
+#     return result
