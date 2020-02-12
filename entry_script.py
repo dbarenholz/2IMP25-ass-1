45d94dda8459ac0bda9a88c94d203b14a74e8a61
Barenholz D <d.barenholz@student.tue.nl>
2020-02-12 16:59:19 +0100

    Clean entry script

--- entry_script.py
+++ entry_script.py
@@ -1,10 +1,5 @@
 #!/usr/bin/env python3
-from helpers import retrieve_match_type
-from helpers import preprocess
-from helpers import retrieve_master_vocab
-from helpers import get_vector_representation
-from helpers import compute_similarity_matrix
-from helpers import get_linked_requirements
+from helpers import retrieve_match_type, preprocess, retrieve_master_vocab, get_vector_representation, compute_similarity_matrix, get_linked_requirements
 from input_output import write_csv
 
 if __name__ == "__main__":
@@ -12,28 +7,27 @@ if __name__ == "__main__":
     Entry point for the script.
 
     Input:
-        None.
+        match_type: the type of matching to be done by the tool.
     Output:
-        None.
-
-    TODO: Update `__main__` documentation.
+        terminal: Some helpful messages on the usage of the program.
+        files: a `links.csv` file in `/output` containing the links found, based on the match_type.
     """
     # Retrieve current match type
     match_type, match_type_explanation = retrieve_match_type()
 
     # Read in high and low level requirements from CSV
-    low_csv_in = r'/input/low.csv'
-    high_csv_in = r'/input/high.csv'
+    low_csv_in = r'/input/low.csv'       # CSV file containing low level requirements.
+    high_csv_in = r'/input/high.csv'     # CSV file containing high level requirements.
 
     # Preprocess the CSV file
-    low_level = preprocess(low_csv_in)   # dict({id : [word, word]})
-    high_level = preprocess(high_csv_in) # dict({id : [word, word]})
+    low_level = preprocess(low_csv_in)   # dict({id : [word, word, ...]})
+    high_level = preprocess(high_csv_in) # dict({id : [word, word, ...]})
 
     # Retrieve the master vocabulary
     master_vocab = set(retrieve_master_vocab(low_level, high_level))
     
     # Create requirements dictionary
-    requirements = {**low_level, **high_level}
+    requirements = {**low_level, **high_level} # Python 3.5+ syntax for joining dictionaries properly.
 
     # Create vector representation for requirements
     vectors = get_vector_representation(master_vocab, requirements)
@@ -45,5 +39,5 @@ if __name__ == "__main__":
     linked_requirements = get_linked_requirements(match_type, similarity, high_level, low_level)
 
     # Write linked requirements to output file
-    links_output = r'/output/links.csv'
+    links_output = r'/output/links.csv'  # CSV file that will contain the outputted links
     write_csv(links_output, linked_requirements)
