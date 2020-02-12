6c7fb4e0cc08f2a7ace28f71a0b10844a54fd406
Barenholz D <d.barenholz@student.tue.nl>
2020-02-12 17:33:13 +0100

    Clean io

--- input_output.py
+++ input_output.py
@@ -3,14 +3,14 @@ Module for dealing with IO related tasks.
 Has methods for reading and writing CSVs.
 """
 
-from typing import List
 import csv
 import sys
+from typing import Dict, List
 
-# Reads CSV files (including headers)
+
 def read_csv(path: str) -> List[str]:
     """
-    Reads a csv file located at path, and returns a list of items.
+    Reads a csv file located at path, and returns a list of lines.
     Exits on errors.
     """
 
@@ -24,24 +24,27 @@ def read_csv(path: str) -> List[str]:
             reader = csv.reader(f)
             # Attempt to read each line.
             try:
+                # Done on a per-line basis for proper error handling.
                 for row in reader:
                     lines.append(row)
             except csv.Error as e:
                 sys.exit(f'Error reading line {reader.line_num} from file at {path}:\n{e}')
     except Exception as err:
         sys.exit(f'Something went wrong during CSV reading.\n{err}')
+    # Return lines from CSV file
     return lines
 
 
-def write_csv(path, links):
+def write_csv(path: str, links: Dict[str, List[str]]) -> None:
     """
     Writes the output to a file using the python csv writer.
     """
+    # Attempt to open the path
     try:
-        fieldnames = ["id", "links"]
         with open(path, 'w') as f:
+            # Create a writer with specific settings
             writer = csv.writer(f, delimiter=',', quoting=csv.QUOTE_ALL)
-            writer.writerow(fieldnames)
+            writer.writerow(["id", "links"])
             writer.writerows([k, ','.join(v)] for (k , v) in links.items())
     except Exception as err:
         sys.exit(f'Something went wrong during CSV writing.\n{err}')
